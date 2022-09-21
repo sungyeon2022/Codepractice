ba37bb2b601f66c9fdb54346cdfc2d3c2a4a2fed
josungyeon <zounsam@naver.com>
2022-09-21 11:48:57 +0900

    lv5

--- Python2/Top K Frequent Elements.py
+++ Python2/Top K Frequent Elements.py
@@ -4,19 +4,18 @@ class Solution(object):
         freq =[[]for i in range(len(nums)+1)]
         for n in nums:
             count[n] = 1 + count.get(n, 0)
-        for n, c in count.items():
-            freq[c].append(n)
-
+        print(count)
         res = []
         for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
                 res.append(n)
                 if len(res) == k:
                     return res
-        print(res)
 
 
-a = [1,1,1,2,2,3]
-b = 2
+
+
+a = [3,0,1,0]
+b = 1
 sc = Solution()
-sc.topKFrequent(a, b)
+print(sc.topKFrequent(a, b))
